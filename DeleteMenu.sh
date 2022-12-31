347ab1dc8b4fd5cc092364a2020362dc411ffab9
norhan-khaled99 <61085153+norhan-khaled99@users.noreply.github.com>
2022-12-31 11:57:35 +0200

    Add files via upload
    add delete menu

---
+++ DeleteMenu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+  echo "1. delete All Data from Table"
+  echo "2. delete specific Data from Table"
+  echo "3. back to Table Menu"
+  echo "4. Exit "
+  echo $PWD
+  read -p "Enter Choice:" ch
+
+  case $ch in
+  
+    1)  bash ../../DeleteALL.sh ;;
+    2)  bash ../../deleteFromTable.sh ;;
+    3)  bash ../../tableMenu.sh ;;
+    4)  ;;
+    *) echo " Wrong Choice " ;;
+  esac
+
