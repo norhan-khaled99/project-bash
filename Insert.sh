eadd8aee3184bc3ad3cf51b890e277a3c59817aa
Heba ibrahim <116721543+hebaibrahimahmed@users.noreply.github.com>
2022-12-31 23:11:16 +0200

    Add files via upload
    Add InsertintoTable File

---
+++ Insert.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+function insert {
+  read -p "Table Name: " tableName
+  if ! [[ -f $tableName ]]; then
+    echo "Table $tableName doesn't exist "
+    bash ../../tableMenu.sh
+    return;
+  fi
+  rowsNum=`awk 'END{print NR}' .$tableName`
+  sep="|"
+  rSep="\n"
+  for (( i = 2; i <= $rowsNum; i++ )); do
+    colName=$(awk 'BEGIN{FS="|"}{ if(NR=='$i') print $1}' .$tableName)
+    colType=$( awk 'BEGIN{FS="|"}{if(NR=='$i') print $2}' .$tableName)
+    colKey=$( awk 'BEGIN{FS="|"}{if(NR=='$i') print $3}' .$tableName)
+    echo -e "$colName ($colType) = \c"
+    read data
+
+    # Validate Input
+    if [[ $colType == "int" ]]; then
+      while ! [[ $data =~ ^[0-9]*$ ]]; do
+        echo -e "invalid DataType !!"
+        echo -e "$colName ($colType) = \c"
+        read data
+      done
+    fi
+    if [[ $colType == "str" ]]; then
+      while ! [[ $data =~ ^[a-zA-Z]*$ ]]; do
+        echo -e "invalid DataType !!"
+        echo -e "$colName ($colType) = \c"
+        read data
+      done
+    fi
+
+    if [[ $colKey == "PK" ]]; then
+      while [[ true ]]; do
+        if [[ $data =~ ^[`awk 'BEGIN{FS="|" ; ORS=" "}{if(NR != 1)print $(('$i'-1))}' $tableName`]$ ]]; then
+          echo -e "invalid input for Primary Key !!"
+        else
+          break;
+        fi
+        read -p "$colName ($colType) =  " data
+      done
+    fi
+
+    #Set row
+    if [[ $i == $rowsNum ]]; then
+      row=$row$data$rSep
+    else
+      row=$row$data$sep
+    fi
+  done
+  echo -e $row"\c" >> $tableName
+  if [[ $? == 0 ]]
+  then
+    echo "Data Inserted Successfully"
+  else
+    echo "Error Inserting Data into Table $tableName"
+  fi
+  row=""
+  bash ../../tableMenu.sh
+}
+
+insert
+
