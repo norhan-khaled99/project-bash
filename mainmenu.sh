e57a80aecf2d39b94630fc5787e4c5b931c806a3
norhan-khaled99 <61085153+norhan-khaled99@users.noreply.github.com>
2023-01-03 20:41:55 +0200

    final upadate for mainmenu with colors

--- mainmenu.sh
+++ mainmenu.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
-#function mainMenu {
-  echo "//////////////////////////////////"
+  echo -e "\e[1;36m  "
   echo "1. Create DataBase"
   echo "2. List the DataBases "
   echo "3. Connect to the clearDataBase"
   echo "4. Drop DataBase"
   echo "5. Exit"
-  echo "//////////////////////////////////"
- 
-   echo -e "Enter Choice: \c"
+   
+   echo -e "\e[0;34m  "
+   read -p "Enter Choice:"
   read ch
   case $ch in
     1) bash createDB.sh ;;
@@ -17,7 +15,6 @@
     3)  connectDB ;;
     4)  dropDB ;;
     5)  ;;
-    *) echo " Wrong option " ;; 
+    *) echo " Wrong option " ;bash mainmenu.sh; 
   esac
-#}
 
