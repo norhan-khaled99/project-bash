dd922c99f96cc0c093d3b8cceb45732db6b0813e
norhan-khaled99 <61085153+norhan-khaled99@users.noreply.github.com>
2023-01-03 23:40:37 +0200

    final update to select row

--- selectrow.sh
+++ selectrow.sh
@@ -2,13 +2,23 @@
 export LC_COLLATE=C
 shopt -s extglob
 
-read -p "Enter Table Name: " tName
+ read -p "Enter Table Name: " tName
  
   if ! [ -f $tName ]
 	then
 	echo "Table $tName doesn't exist"
 	bash ../../tableMenu.sh 
   fi
- read -p "Enter value: " value
-row=$(grep -n "$value" $tName | cut -d: -f1)
-sed -n "${row}p" $tName
+
+read -p "Enter the value to check: " VALUE
+
+  
+if [ -n "$(sed -n "/$VALUE/p" $tName)" ]; then
+    sed -n "/$VALUE/p" $tName
+     
+else 
+   echo "value not found"
+   
+fi
+  
+bash ../../tableMenu.sh 
