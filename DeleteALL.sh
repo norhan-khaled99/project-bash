8ccab055eac589b56af29fa4fd394d5abaff690a
norhan-khaled99 <61085153+norhan-khaled99@users.noreply.github.com>
2023-01-03 17:22:46 +0200

    add delete all

---
+++ DeleteALL.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export LC_COLLATE=C
+shopt -s extglob
+read -p "Enter the  Table Name You want to delete:" table_Name
+   
+  if [[ -f $table_Name ]]
+  then
+
+   sed -i '2,$d' ./$table_Name
+   echo "row deleted successfully" 
+  else 
+    echo "Table $table_Name doesn't exist"
+  fi
+   bash ../../DeleteMenu.sh
+
+
