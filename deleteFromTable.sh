eb90baf35cda4032a64d4e4535486a36dc489525
norhan-khaled99 <61085153+norhan-khaled99@users.noreply.github.com>
2023-01-03 17:24:26 +0200

    delete row from table

---
+++ deleteFromTable.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export LC_COLLATE=C
+#shopt -s extglob
+
+ read -p "Enter Table Name: " tName
+ 
+  if ! [ -f $tName ]
+	then
+	echo "Table $tName doesn't exist"
+	bash ../../tableMenu.sh 
+  fi
+
+    read -p "Enter value: " value
+row=$(grep -n "$value" $tName | cut -d: -f1)
+sed -i "${row}d" $tName
+     bash ../../tableMenu.sh
+    fi
+  fi
